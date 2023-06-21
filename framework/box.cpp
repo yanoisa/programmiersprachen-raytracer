834cd7d391319943a838aba6c81aa38f72a0c019
Patricia Mangold <patricia.mangold@uni-weimar.de>
2023-06-21 13:42:31 +0200

    Aufgabe 5.2 Box Tests

--- framework/box.cpp
+++ framework/box.cpp
@@ -1,18 +1,18 @@
 #include <glm/vec3.hpp>
 #include "box.hpp"
-
+#include <cmath>
 Box::Box() :min_{}, max_{} {}
 Box::Box(glm::vec3 const& min, glm::vec3 const& max) :min_{ min }, max_{ max } {}
 
 float Box::area()const {
-	float x = max_.x - min_.x;
-	float y = max_.y - min_.y;
-	float z = max_.z - min_.z;
+	float x = std::abs(max_.x - min_.x);
+	float y = std::abs(max_.y - min_.y);
+	float z = std::abs(max_.z - min_.z);
 	return 2 * x * z + 2 * x * y + 2 * y * z;
 }
 float Box::volume()const {
 	float x = max_.x - min_.x;
 	float y = max_.y - min_.y;
 	float z = max_.z - min_.z;
-	return x * y * z;
+	return std::abs(x * y * z);
 }
--- source/tests.cpp
+++ source/tests.cpp
@@ -5,7 +5,7 @@
 #include "sphere.hpp"
 
 
-TEST_CASE("sphereconstruct", "[sphereconstruct]") {
+TEST_CASE("sphere", "[sphere]") {
 	Sphere sphere(glm::vec3(0.0f, 0.0f, 0.0f), 0.0f);
 	Sphere sphere1 = Sphere();
 	Sphere sphere2(glm::vec3(5.0f, 3.0f, 20.0f), 3.0f);
@@ -18,7 +18,21 @@ TEST_CASE("sphereconstruct", "[sphereconstruct]") {
 	REQUIRE(sphere2.volume() ==Approx(113.097));
 	REQUIRE(sphere3.area() == Approx(40.715));
 	REQUIRE(sphere3.volume() == Approx(24.429));
+}
 
+TEST_CASE("box", "[box]") {
+	Box box = Box();
+	Box box1(glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 0.0f, 0.0f));
+	Box box2(glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(1.0f, 4.0f, 2.0f));
+	Box box3(glm::vec3(5.0f, 1.0f, 10.0f), glm::vec3(2.0f, 3.0f, 2.0f));
+	REQUIRE(box.area() == 0);
+	REQUIRE(box.volume() == 0);
+	REQUIRE(box1.area() == 0);
+	REQUIRE(box1.volume() == 0);
+	REQUIRE(box2.area() == 28.0f);
+	REQUIRE(box2.volume() == 8.0f);
+	REQUIRE(box3.area() == 92.0f);
+	REQUIRE(box3.volume() == 48.0f);
 }
 int main(int argc, char *argv[])
 {
