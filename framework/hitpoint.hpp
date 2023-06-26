f84117f6a241e3c9301ac86070cd5777451c503e
Patricia Mangold <patricia.mangold@uni-weimar.de>
2023-06-26 08:08:46 +0200

    Intersect Methode nun vollständig

---
+++ framework/hitpoint.hpp
@@ -0,0 +1,14 @@
+# ifndef HITPOINT_HPP
+# define HITPOINT_HPP
+#include <glm/glm.hpp>
+struct HitPoint
+{
+	bool cut;
+	float distance;
+	std::string name;
+	Color color;
+	glm::vec3 point;
+	glm::vec3 direction;
+};
+
+#endif
